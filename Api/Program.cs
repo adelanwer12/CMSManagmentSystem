85e6a6d47bfa63f2271b01444a95bd2c2fc0fa87
Adel Ibrahim <adelanwer12@outlook.com>
2021-05-18 10:49:45 +0200

    implement serilog

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using loggingService;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Api
 {
@@ -13,13 +18,39 @@ namespace Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host =CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    int zero = 0;
+                    int result = 100/ zero;
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Log.Error("An error occurred while seeding database {Error} {StackTrace} {InnerException} {Source}", ex.Message,ex.StackTrace,ex.InnerException,ex.Source);
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                      .Enrich.FromLogContext().Enrich.WithProperty("Application", "Api").Enrich
+                             .WithProperty("MachineName", Environment.MachineName).Enrich
+                             .WithProperty("CurrentManagedThreadId", Environment.CurrentManagedThreadId).Enrich
+                             .WithProperty("OSVersion", Environment.OSVersion).Enrich
+                             .WithProperty("Version", Environment.Version).Enrich
+                             .WithProperty("UserName", Environment.UserName).Enrich
+                             .WithProperty("ProcessId", Process.GetCurrentProcess().Id).Enrich
+                             .WithProperty("ProcessName", Process.GetCurrentProcess().ProcessName)
+                             .WriteTo.Console(theme: CustomConsoleTheme.VisualStudioMacLight)
+                             .WriteTo.File(formatter: new CustomTextFormatter(), path: Path.Combine(hostingContext.HostingEnvironment.ContentRootPath + $"{Path.DirectorySeparatorChar}Logs{Path.DirectorySeparatorChar}", $"cms_api_{DateTime.Now:yyyyMMdd}.txt"))
+                             .ReadFrom.Configuration(hostingContext.Configuration));
                     webBuilder.UseStartup<Startup>();
                 });
     }
